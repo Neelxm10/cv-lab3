119839a8ee11dd3b0e94e2a9408a4b8ef8f0ffd8
Neelxm10 <mukh0023@algonquinlive.com>
2024-01-31 13:01:35 -0500

    new commit again

--- lab3.py
+++ lab3.py
@@ -4,5 +4,18 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import random
 
+##  BIG MICH
+
+#Read in image
+
+#convert RGB to HSV
+
+#Use subplots from matplotlib to show HSV and original photo
 
 
+#Seperate channels Hue (H), Saturation (S), Value (V)
+
+
+
+#Apply masking on the most appropriate image channel (H,S, or V)
+Channel = np.ones(np.array(0,imgChannelSize))

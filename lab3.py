858f9283a14eead5ba755cf07b89fdf786f503df
LeMich-A <makpro2002@gmail.com>
2024-01-31 15:57:30 -0500

    ey

--- lab3.py
+++ lab3.py
@@ -15,7 +15,7 @@ import random
 
 #Seperate channels Hue (H), Saturation (S), Value (V)
 
-path = r'/home/mich/Documents/Michel/Opencv/Labs/cv-lab3/leaves.jpg'
+path = r'/home/mich/Documents/Michel/Opencv/cv-lab3/leaves.jpg'
 img = cv.imread(path)
 
 
@@ -26,29 +26,27 @@ imgSat = imgHSV[:, :, 1]
 imgVal= imgHSV[:, :, 2]
 
 
-fig, axes = plt.subplots(2, 5, figsize=(10, 10))
+fig, axes = plt.subplots(2, 4, figsize=(10, 10))
 
-# Original Image
-axes[0, 0].imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-axes[0, 0].set_title('Original Image')
+
 
 # HSV Image
-axes[0, 1].imshow(cv.cvtColor(imgHSV, cv.COLOR_HSV2RGB))
-axes[0, 1].set_title('HSV Image')
+axes[0, 0].imshow(imgHSV, cmap='hsv')
+axes[0, 0].set_title('HSV Image')
 
 # Hue Channel
-axes[0, 2].imshow(imgHue, cmap='hsv')
-axes[0, 2].set_title('Hue Channel')
+axes[0, 1].imshow(imgHue, cmap='hsv')
+axes[0, 1].set_title('Hue Channel')
 
 # Saturation Channel
-axes[0, 3].imshow(imgSat, cmap='gray')
-axes[0, 3].set_title('Saturation Channel')
+axes[0, 2].imshow(imgSat, cmap='gray')
+axes[0, 2].set_title('Saturation Channel')
 
 # Value Channel
 # Normalize the Value channel to be between 0 and 1
 imgVal_normalized = imgVal / 255.0
-axes[0, 4].imshow(imgVal_normalized, cmap='gray')
-axes[0, 4].set_title('Value Channel')
+axes[0, 3].imshow(imgVal_normalized, cmap='gray')
+axes[0, 3].set_title('Value Channel')
 
 # Remove axis labels
 for ax in axes.flatten():
